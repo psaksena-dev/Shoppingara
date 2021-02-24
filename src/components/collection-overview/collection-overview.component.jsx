f54ba6e315b6a73dfc2b6a7bbdb9e7f1b784e2a4
Pulkit Saxena <psaksena85@gmail.com>
2021-02-24 04:48:57 -0500

    New flow

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './collection-overview.styles.scss';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { selectCollections } from './../../redux/shop/shop.selector';
+import { shopCollectionsForPreview } from './../../redux/shop/shop.selector';
 import CollectionPreview from './../../components/collection-preview/collection-preview.component';
 
 const CollectionOverview = ({ collections }) => {
@@ -16,7 +16,7 @@ const CollectionOverview = ({ collections }) => {
 };
 
 const mapStateToProps = createStructuredSelector({
-	collections: selectCollections
+	collections: shopCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import CollectionOverview from './../../components/collection-overview/collection-overview.component';
+import CollectionPage from './../../pages/collection/collection.component';
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
 	<div>
-		<CollectionOverview />
+		<Route exact path={`${match.path}`} component={CollectionOverview} />
+		<Route path={`${match.path}/:collectionId`} component={CollectionPage} />
 	</div>
 );
 

da7ba33d6cce8a4cd57dd828a606613717892727
Pulkit Saxena <psaksena85@gmail.com>
2021-03-27 01:33:39 -0400

    change

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ const port = process.env.PORT || 6000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(cors());
-
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, 'client/build')));
 
